de22cfbf0937b077279a2e69435cd6215f115131
EditSIMS <adamyousif854@gmail.com>
2025-02-21 21:49:28 +0300

    Update package.py
    new repeat feature

--- lib/package.py
+++ lib/package.py
@@ -8,7 +8,6 @@ import supervisor
 import digitalio
 import os
 import json
-import storage
 import microcontroller
 
 special_keys_upper = {k.upper(): v for k, v in usb.special.items()}
@@ -22,10 +21,9 @@ led.direction = digitalio.Direction.OUTPUT
 def send(this_input, sleep=0.05):
     for item in this_input:
         if type(item) is str:
-            print(item)
             args = item.split(" ")
             if args[0] == "delay":
-                time.sleep(float(args[1]))
+                time.sleep(int(args[1]) / 1000)
         elif type(item) is list:
             kbd.send(*item)
         else:
@@ -42,51 +40,65 @@ def compare_dicts(dict1, dict2):
 
     return True
 
+def get_key(key : str):
+    
+    payload = []
+    errors = []
+
+    if "+" in key:
+        strokes = []
+                
+        # multiple strokes
+        for stroke in key[1:].split("+"):
+            s = stroke
+            if len(stroke) > 1:
+                s = stroke.upper()
+
+            k = command_keys.get(s, None)
+                    
+            if type(k) == list:
+                strokes += k
+            elif type(k) == int:
+                strokes.append(int(k))
+            else: errors.append(f"INVALID STROKE: {stroke}")
+            
+        payload.append(strokes)
+    # single stroke
+    else:
+        stroke = key[1:]
+        if len(stroke) > 1:
+            stroke = stroke.upper()
+                
+        k = command_keys.get(stroke, None)
+                
+        if type(k) == list:
+            payload += k
+        elif type(k) == int:
+            payload.append(k)
+        else: errors.append(f"INVALID STROKE: {stroke}")
+    
+    return payload, errors
+
 def process_commands(buf : str):
     if len(buf) < 1: return [], ["Buffer too short!\n"]
     elif buf[-1] != ";": return [], ["Bad message\n"]
     
     commands = buf.split(";")
-    commands = [cmd for cmd in commands if cmd != ""]
+    commands = [cmd.strip() for cmd in commands if cmd != ""]
     payload = []
     
     errors = []
     
     for command in commands:
         # key commands
+        
+        
         if command.startswith("!") and not command.startswith("!!"):
-            if "+" in command:
-                strokes = []
-                
-                # multiple strokes
-                for stroke in command[1:].split("+"):
-                    s = stroke
-                    if len(stroke) > 1:
-                        s = stroke.upper()
-
-                    k = command_keys.get(s, None)
-                    
-                    if type(k) == list:
-                        strokes += k
-                    elif type(k) == int:
-                        strokes.append(int(k))
-                    else: errors.append(f"INVALID STROKE: {stroke}")
+            k_payload, k_errors = get_key(command)
+        
+            payload += k_payload
             
-                payload.append(strokes)
-            # single stroke
-            else:
-                stroke = command[1:]
-                if len(stroke) > 1:
-                    stroke = stroke.upper()
-                
-                k = command_keys.get(stroke, None)
-                
-                if type(k) == list:
-                    payload += k
-                elif type(k) == int:
-                    payload.append(k)
-                else: errors.append(f"INVALID STROKE: {stroke}")
-        # special commands
+            errors += k_errors
         elif command.startswith("!!"):
             args = command[2:].split(" ")
             # Delay command
@@ -96,7 +108,7 @@ def process_commands(buf : str):
                 ms = None
                 try:
                     ms = int(args[1])  
-                    payload.append(f"delay {ms / 1000}")
+                    payload.append(f"delay {ms}")
                     
                 except:
                     errors.append("INVALID DELAY")
@@ -107,10 +119,43 @@ def process_commands(buf : str):
                     try:
                         val = int(hex_string, 16)
                         payload.append(val)
-                    except:
+                    except ValueError:
                         errors.append(f"INVALID HEX: {args[1]}")
+            elif args[0] == "repeat" and len(args) == 4:
+                delay = None
+                repetitions = None
+                try:
+                    repetitions = int(args[1])
+                except ValueError:
+                    errors.append(f"INVALID REPETITIONS: {arg[1]}")
+                    
+                try:
+                    delay = int(args[2])
+                except ValueError:
+                    errors.append(f"INVALID DELAY: {arg[2]}")
+                
+                if delay and repetitions:
+                    rep_commands = args[3]
+                    
+                    rep_commands = rep_commands.split(";")
+                    rep_commands = [cmd.strip() for cmd in rep_commands if cmd != ""]
+                    
+                    rep_payload = []
+                    
+                    for r_command in rep_commands:
+                        if r_command.startswith("!") and not r_command.startswith("!!"):
+                            k_payload, k_errors = get_key(r_command)
+                            
+                            rep_payload += k_payload
+                            rep_payload.append(f"delay {delay}")
+                            errors += k_errors
+                        else:
+                            rep_payload += usb.get_sequence(r_command)
+                            rep_payload.append(f"delay {delay}")
+                                
+                    payload += (rep_payload * repetitions)[:-1]
             else:
-                errors.append(f"INVALID COMMAND: {args[0]}")
+                errors.append(f"MISSING ARGUMENT FOR {arg[0]}")
         # custom text
         else:
             payload += usb.get_sequence(command)
